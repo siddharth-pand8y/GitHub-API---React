d63afb13a213e101bac8d0f33b539058d8dd6513
Siddharth Pandey <siddharth.pand8y@gmail.com>
2019-01-24 01:38:21 +0530

    Added bio information on User page

--- src/Components/User.js
+++ src/Components/User.js
@@ -77,6 +77,10 @@ class User extends Component {
                 <p className="username-text">{user.login}</p>
               </div>
 
+              <div className="bio-container">
+                <p className="bio-text">{user.bio}</p>
+              </div>
+
               <ul className="user-work-info">
                 <li>
                   {user.company && (
@@ -95,27 +99,6 @@ class User extends Component {
                         />
                       </svg>
                       <p>{user.company}</p>
-                    </div>
-                  )}
-                </li>
-                <li>
-                  {user.blog && (
-                    <div className="user-blog">
-                      <svg
-                        alt="Personal site icon"
-                        className="octicon octicon-link"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="16"
-                        height="16"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-                        />
-                      </svg>
-                      <a href={user.blog}>{user.blog}</a>
                     </div>
                   )}
                 </li>
@@ -139,6 +122,7 @@ class User extends Component {
                     </div>
                   )}
                 </li>
+
                 <li>
                   {' '}
                   {user.email && (
@@ -157,6 +141,27 @@ class User extends Component {
                         />
                       </svg>
                       <a href={user.email}>{user.email}</a>
+                    </div>
+                  )}
+                </li>
+                <li>
+                  {user.blog && (
+                    <div className="user-blog">
+                      <svg
+                        alt="Personal site icon"
+                        className="octicon octicon-link"
+                        viewBox="0 0 16 16"
+                        version="1.1"
+                        width="16"
+                        height="16"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+                        />
+                      </svg>
+                      <a href={user.blog}>{user.blog}</a>
                     </div>
                   )}
                 </li>
